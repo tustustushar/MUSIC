5a65b2f4a4dfd3c33fb65b7d55681fe318aa216f
tustustushar <71062376+tustustushar@users.noreply.github.com>
2021-10-02 00:41:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "")
 admins = {}
+PMPERMIT = getenv("PMPERMIT", None)
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
